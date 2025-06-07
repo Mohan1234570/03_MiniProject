d1ee350f2ac54f18a0df3b5797f9b903745ded9a
GMK <mk4400320@gmail.com>
2025-06-07 15:41:25 +0530

    Updated code with some best practices and code structure in this project

--- src/main/java/com/ait/binding/PostForm.java
+++ src/main/java/com/ait/binding/PostForm.java
@@ -15,37 +15,37 @@ public class PostForm {
 	
 	@Lob
 	private String content;
-
-	public Integer getBlogId() {
-		return blogId;
-	}
-
-	public void setBlogId(Integer blogId) {
-		this.blogId = blogId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
+//
+//	public Integer getBlogId() {
+//		return blogId;
+//	}
+//
+//	public void setBlogId(Integer blogId) {
+//		this.blogId = blogId;
+//	}
+//
+//	public String getTitle() {
+//		return title;
+//	}
+//
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//
+//	public String getContent() {
+//		return content;
+//	}
+//
+//	public void setContent(String content) {
+//		this.content = content;
+//	}
 
 }
